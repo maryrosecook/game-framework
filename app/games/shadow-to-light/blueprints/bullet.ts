214912098e71ea4a50a5cc67e0474366a75e13e9
Mary Rose Cook <mary@maryrosecook.com>
2025-11-25 11:38:06 -0800

    bullets destroy shadow-minions

--- app/games/shadow-to-light/blueprints/bullet.ts
+++ app/games/shadow-to-light/blueprints/bullet.ts
@@ -1,6 +1,8 @@
 import { BlueprintData, RuntimeGameState, RuntimeThing, KeyState } from "@/engine/types";
 
 export default function createBlueprint24(data: BlueprintData) {
+  const TARGET_BLUEPRINT = "shadow-minion";
+
   return {
     ...data,
     input: (thing: RuntimeThing, _state: RuntimeGameState, _keys: KeyState) => {
@@ -8,6 +10,19 @@ export default function createBlueprint24(data: BlueprintData) {
     },
     update: (thing: RuntimeThing, _state: RuntimeGameState, _things: RuntimeThing[]) => {
       return thing;
+    },
+    collision: (
+      thing: RuntimeThing,
+      other: RuntimeThing,
+      gameState: RuntimeGameState
+    ) => {
+      if (other.blueprintName !== TARGET_BLUEPRINT) {
+        return;
+      }
+
+      gameState.things = gameState.things.filter(
+        (candidate) => candidate.id !== other.id && candidate.id !== thing.id
+      );
     },
     render: (thing: RuntimeThing, _state: RuntimeGameState, ctx: CanvasRenderingContext2D) => {
       ctx.fillStyle = thing.color;
