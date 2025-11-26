62952aacbec6b40ab4aeb62a1739d6e90eba58b0
Mary Rose Cook <mary@maryrosecook.com>
2025-11-25 21:24:19 -0800

    Make shadow minions lethal on contact (#2)

--- app/games/shadow-to-light/blueprints/shadow-minion.ts
+++ app/games/shadow-to-light/blueprints/shadow-minion.ts
@@ -1,5 +1,7 @@
 import { BlueprintData, RuntimeGameState, RuntimeThing, KeyState } from "@/engine/types";
 
+const LETHAL_TARGETS = new Set(["player", "wolf"]);
+
 export default function createBlueprint2(data: BlueprintData) {
   return {
     ...data,
@@ -8,6 +10,15 @@ export default function createBlueprint2(data: BlueprintData) {
     },
     update: (thing: RuntimeThing, _state: RuntimeGameState, _things: RuntimeThing[]) => {
       return thing;
+    },
+    collision: (_thing: RuntimeThing, other: RuntimeThing, gameState: RuntimeGameState) => {
+      if (!LETHAL_TARGETS.has(other.blueprintName)) {
+        return;
+      }
+
+      gameState.things = gameState.things.filter(
+        (candidate) => candidate.id !== other.id
+      );
     },
     render: (thing: RuntimeThing, _state: RuntimeGameState, ctx: CanvasRenderingContext2D) => {
       ctx.fillStyle = thing.color;
