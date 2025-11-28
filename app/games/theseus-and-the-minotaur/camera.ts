05f1e9c2b1eaff073b9f628d93ba857b31dc444e
Mary Rose Cook <mary@maryrosecook.com>
2025-11-28 07:25:58 -0800

    Center camera on Theseus (#16)
    * Make camera follow Theseus
    
    * Restore Theseus start position

--- app/games/theseus-and-the-minotaur/camera.ts
+++ app/games/theseus-and-the-minotaur/camera.ts
@@ -1,5 +1,22 @@
-import { RuntimeGameState } from "@/engine/types";
+import { RuntimeGameState, Vector } from "@/engine/types";
 
-export function update(game: RuntimeGameState) {
-  return game.camera;
+export function update(game: RuntimeGameState): Vector {
+  const theseus = game.things.find(
+    (thing) => thing.blueprintName === "theseus"
+  );
+  if (!theseus) {
+    return game.camera;
+  }
+
+  const center = {
+    x: theseus.x + theseus.width / 2,
+    y: theseus.y + theseus.height / 2,
+  };
+
+  return {
+    x: center.x - game.screen.width / 2,
+    y: center.y - game.screen.height / 2,
+  };
 }
+
+export default { update };
