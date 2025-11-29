63ecbdd3c6138a756fc7248d2a7ce2ff6d3d81fd
Mary Rose Cook <mary@maryrosecook.com>
2025-11-29 08:26:00 -0800

    add initial thomas (#25)

---
+++ app/games/thomas-air-track/blueprints/pixel-art-1764363609520-yfak4iz2e.ts
@@ -0,0 +1,17 @@
+import { BlueprintData, RuntimeGameState, RuntimeThing, KeyState } from "@/engine/types";
+
+export default function createPixelArt1764363609520Yfak4iz2e(data: BlueprintData) {
+  return {
+    ...data,
+    input: (thing: RuntimeThing, _state: RuntimeGameState, _keys: KeyState) => {
+      return thing;
+    },
+    update: (thing: RuntimeThing, _state: RuntimeGameState, _things: RuntimeThing[]) => {
+      return thing;
+    },
+    render: (thing: RuntimeThing, _state: RuntimeGameState, ctx: CanvasRenderingContext2D) => {
+      ctx.fillStyle = thing.color;
+      ctx.fillRect(0, 0, thing.width, thing.height);
+    },
+  };
+}
